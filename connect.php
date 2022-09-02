ca059a7d2a19e28a04e4cfcd06b211939c4225aa
dev-harrisonw <harrisonwarby111@gmail.com>
2022-09-02 15:56:00 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,9 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:londoncomedylunch.database.windows.net,1433; Database = LondonComedyLunch", "Inconnection_LCL", "Golfer70!");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "Inconnection_LCL", "pwd" => "Golfer70!", "Database" => "LondonComedyLunch", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:londoncomedylunch.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+$con=new mysqli('tcp:londoncomedylunch.database.windows.net,1433','Inconnection_LCL','Golfer70!','LCL');//it connects to your server. positioning is matters.
+
+if(!$con){
+    die(mysqli_error($con));//if server has any problem it will show the error
+}
 ?>
