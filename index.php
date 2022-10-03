7ce724b361585b2047e6e09d031eb5cd6398b72a
dev-harrisonw <harrisonwarby111@gmail.com>
2022-10-03 16:37:10 +0100

    main

--- index.php
+++ index.php
@@ -1,87 +1,19 @@
-
 <?php
-include "connect.php";
-
+    $serverName = "londoncomedylunch.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "LondonComedyLunch", // update me
+        "Uid" => "Inconnection_LCL", // update me
+        "PWD" => "Golfer70!" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT * FROM LCL";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['TableNo'] . " " . $row['Status'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="index.css">
-</head>
-<body>
-    <div class="cricles">
-   
-<p> Hello World!! </p>
-    
-
-<?php
-
-function mysql_query_or_die($query) {
-    $result = mysql_query($query);
-    if ($result)
-        return $result;
-    else {
-        $err = mysql_error();
-        die("<br>{$query}<br>*** {$err} ***<br>");
-    }
-}
-
-$query = "SELECT * FROM LCL";
-$result = mysql_query_or_die($query);
-echo("<table>");
-$first_row = true;
-while ($row = mysql_fetch_assoc($result)) {
-    if ($first_row) {
-        $first_row = false;
-        // Output header row from keys.
-        echo '<tr>';
-        foreach($row as $key => $field) {
-            echo '<th>' . htmlspecialchars($key) . '</th>';
-        }
-        echo '</tr>';
-    }
-    echo '<tr>';
-    foreach($row as $key => $field) {
-        echo '<td>' . htmlspecialchars($field) . '</td>';
-    }
-    echo '</tr>';
-}
-echo("</table>");
-
-?>
-
-<!--- <?php 
-
-
-$sql="select * from `LCL`";
-$result=mysqli_query($conn,$sql);
-
-
-if($result){
-    while( $row=mysqli_fetch_assoc($result)){
-        $id=$row['id'];
-        $TableNo=$row['TableNo'];
-        $Status=$row['Status'];
-        $Category=$row['Category'];
-        echo '
-        <div class="circle" data-status='.$Status.'>
-        <p class="name">'.$TableNo.'</p>
-        <p class="category">'.$Category.'</p>
-
-    </div>
-        ';
-    }
-
-}
-?> --->
-
-</div>
-</body>
-
-<script src="index.js"></script>
-</html>
